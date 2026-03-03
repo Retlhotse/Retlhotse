561f5a43e4e0aaafba6596cc057da15cd541a6a7
Retlhotse <retlhotsem@gmail.com>
2026-03-03 22:35:41 +0200

    Delete src directory

--- src/tools/search_tool.py
+++
@@ -1,8 +0,0 @@
-from langchain.tools import tool
-from db.weaviate_client import vector_store
-
-@tool
-def search_user_guide(query: str) -> str:
-    """Search user guide documents using semantic similarity."""
-    docs = vector_store.similarity_search(query, k=3)
-    return "\n\n".join([doc.page_content for doc in docs])
